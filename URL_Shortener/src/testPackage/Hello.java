615924fe3df217b4c833cf0cae4df8ebf3f78766
mosiadz2 <47243149+mosiadz2@users.noreply.github.com>
2020-03-07 18:21:57 +0000

    Initial Commit
    Integrated apache tomcat with eclipse and instaled liblaries for REST functionality.
    tested the functionality of the server with simple @GET annotation.

---
+++ URL_Shortener/src/testPackage/Hello.java
@@ -0,0 +1,40 @@
+package testPackage;
+
+//Liblaries
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/hello")
+public class Hello
+{
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHelloHTML()
+	{
+		String response = "<html>" + "<title>" + "Hello Patryk" + "</title>" + "<body><h1>" + "Hello Patryk from HTML" + 
+				"</h1></body>" + "</html>";
+		return response;
+	}
+	
+	  @GET
+	  @Produces(MediaType.TEXT_XML) public String sayHelloXML() 
+	  { 
+		  String response = "<?xml version='1.0'?>" + "<hello>Hi Patryk, this is Hello from XML</hello>";
+		  return response; 
+	  
+	  }
+	  
+	  @GET
+	  @Produces(MediaType.TEXT_PLAIN) public String sayHelloPlain() 
+	  { 
+		  String response = "Hi Patryk, this is Hello from HTML"; 
+		  return response;
+	  }
+	 
+
+	
+	
+}
